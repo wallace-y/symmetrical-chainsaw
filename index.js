d5a09c2791c18964b9212bad8ca50cff4fab5a29
wallace-y <cameronwfinney@gmail.com>
2023-03-31 11:39:26 +0100

    removing console.logs from pushed file

--- index.js
+++ index.js
@@ -69,7 +69,3 @@ app.listen(PORT, function(err) {
     if (err) console.log("Error in server setup")
     console.log("Server listening on Port",PORT); 
 });
-
-console.log(message);
-resetMessage();
-console.log(message);
